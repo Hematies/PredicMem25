43a62d78e4c9efcff1d3612901026fdf0fb9a125
Pablo <pablo-aries-7@hotmail.com>
2024-10-17 10:10:06 +0200

    Se quita el paso explícito por referencia (por si acaso)

--- svm.hpp
+++ svm.hpp
@@ -6,17 +6,17 @@ protected:
 	weight_t weights[NUM_CLASSES][SEQUENCE_LENGTH][NUM_CLASSES_INCLUDING_NULL];
 	weight_t intercepts[NUM_CLASSES];
 
-	void encodeInOneHot(class_t(&classSequence)[SEQUENCE_LENGTH], bool(&oneHotSequence)[SEQUENCE_LENGTH][NUM_CLASSES_INCLUDING_NULL]);
-	void fit(class_t (&input)[SEQUENCE_LENGTH], class_t target);
-	class_t predict(class_t (&input)[SEQUENCE_LENGTH]);
+	void encodeInOneHot(class_t classSequence[SEQUENCE_LENGTH], bool oneHotSequence[SEQUENCE_LENGTH][NUM_CLASSES_INCLUDING_NULL]);
+	void fit(class_t input[SEQUENCE_LENGTH], class_t target);
+	class_t predict(class_t input[SEQUENCE_LENGTH]);
 	
-	distance_t distanceToHyperplane(bool(&oneHotSequence)[SEQUENCE_LENGTH][NUM_CLASSES_INCLUDING_NULL], class_t hyperplane);
+	distance_t distanceToHyperplane(bool oneHotSequence [SEQUENCE_LENGTH][NUM_CLASSES_INCLUDING_NULL], class_t hyperplane);
 public:
 	class_t operator()(bool opPredict, class_t input[SEQUENCE_LENGTH], class_t target);
 };
 
 template<typename weight_t, typename class_t, typename distance_t>
-void SVM<weight_t, class_t, distance_t>::encodeInOneHot(class_t (&classSequence)[SEQUENCE_LENGTH], bool(&oneHotSequence)[SEQUENCE_LENGTH][NUM_CLASSES_INCLUDING_NULL]) {
+void SVM<weight_t, class_t, distance_t>::encodeInOneHot(class_t classSequence[SEQUENCE_LENGTH], bool oneHotSequence[SEQUENCE_LENGTH][NUM_CLASSES_INCLUDING_NULL]) {
 	for (int i = 0; i < SEQUENCE_LENGTH; i++) {
 		for (int j = 0; j < NUM_CLASSES_INCLUDING_NULL; j++) {
 			oneHotSequence[i][j] = classSequence[i] == j;
@@ -25,7 +25,7 @@ void SVM<weight_t, class_t, distance_t>::encodeInOneHot(class_t (&classSequence)
 }
 
 template<typename weight_t, typename class_t, typename distance_t>
-distance_t SVM<weight_t, class_t, distance_t>::distanceToHyperplane(bool(&oneHotSequence)[SEQUENCE_LENGTH][NUM_CLASSES_INCLUDING_NULL], class_t hyperplane) {
+distance_t SVM<weight_t, class_t, distance_t>::distanceToHyperplane(bool oneHotSequence[SEQUENCE_LENGTH][NUM_CLASSES_INCLUDING_NULL], class_t hyperplane) {
 	distance_t res = 0;
 	for (int i = 0; i < SEQUENCE_LENGTH; i++) {
 		for (int j = 0; j < NUM_CLASSES_INCLUDING_NULL; j++) {
@@ -38,7 +38,7 @@ distance_t SVM<weight_t, class_t, distance_t>::distanceToHyperplane(bool(&oneHot
 }
 
 template<typename weight_t, typename class_t, typename distance_t>
-class_t SVM<weight_t, class_t, distance_t>::predict(class_t(&input)[SEQUENCE_LENGTH]) {
+class_t SVM<weight_t, class_t, distance_t>::predict(class_t input [SEQUENCE_LENGTH]) {
 	bool oneHotSequence[SEQUENCE_LENGTH][NUM_CLASSES_INCLUDING_NULL];
 	encodeInOneHot(input, oneHotSequence);
 
@@ -55,7 +55,7 @@ class_t SVM<weight_t, class_t, distance_t>::predict(class_t(&input)[SEQUENCE_LEN
 }
 
 template<typename weight_t, typename class_t, typename distance_t>
-void SVM<weight_t, class_t, distance_t>::fit(class_t(&input)[SEQUENCE_LENGTH], class_t target) {
+void SVM<weight_t, class_t, distance_t>::fit(class_t input[SEQUENCE_LENGTH], class_t target) {
 	bool oneHotSequence[SEQUENCE_LENGTH][NUM_CLASSES_INCLUDING_NULL];
 	encodeInOneHot(input, oneHotSequence);
 
