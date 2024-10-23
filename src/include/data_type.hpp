a0a72cca76ad954a58703d76a9085f6f4d9a3ac9
Hematies <pablo-aries-7@hotmail.com>
2024-10-23 17:58:14 +0200

    Quedan definidos los tipos de los datos, siguiendo el tipado de HLS

--- src/include/data_type.hpp
+++ src/include/data_type.hpp
@@ -1,17 +1,18 @@
 #pragma once
 
 #include "config.hpp"
+#include "ap_int.h"
 
-typedef int address_t;
-typedef int ib_index_t;
-typedef int ib_way_t;
-typedef int ib_tag_t;
-typedef int block_address_t;
-typedef int class_t;
-typedef int ib_confidence_t;
-typedef int ib_lru_t;
-typedef int dic_index_t;
-typedef int delta_t;
-typedef int dic_confidence_t;
-typedef int svm_weight_t;
-typedef int svm_distance_t;
+typedef ap_uint<NUM_ADDRESS_BITS> address_t;
+typedef ap_uint<IB_NUM_SETS_LOG2> ib_index_t;
+typedef ap_uint<IB_NUM_WAYS_LOG2> ib_way_t;
+typedef ap_uint<IB_NUM_TAG_BITS> ib_tag_t;
+typedef ap_uint<NUM_BLOCK_ADDRESS_BITS> block_address_t;
+typedef ap_uint<NUM_CLASSES_INCLUDING_NULL_LOG2> class_t;
+typedef ap_uint<MAX_PREDICTION_CONFIDENCE_LOG2> ib_confidence_t;
+typedef ap_uint<IB_NUM_LRU_COUNTER_BITS> ib_lru_t;
+typedef ap_uint<NUM_CLASSES_LOG2> dic_index_t;
+typedef ap_int<NUM_DELTA_BITS> delta_t;
+typedef ap_uint<DICTIONARY_LFU_CONFIDENCE_LOG2> dic_confidence_t;
+typedef ap_int<NUM_SVM_WEIGHT_BITS> svm_weight_t;
+typedef ap_int<NUM_DISTANCE_BITS> svm_distance_t;
--- src/include/global.hpp
+++ src/include/global.hpp
@@ -2,7 +2,6 @@
 
 #include "config.hpp"
 #include "dictionary.hpp"
-#include "encoder.hpp"
 #include "global.hpp"
 #include "input_buffer.hpp"
 #include "svm.hpp"
