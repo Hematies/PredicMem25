7f046afe8879b2a62078be723914a1652f71d978
Hematies <psanchez4@us.es>
2024-12-05 19:44:06 +0100

    Adaptación de testbench que funciona en la 2022.1

--- src/testbench/main.cpp
+++ src/testbench/main.cpp
@@ -2,25 +2,77 @@
 #include <string>
 #include "../include/global.hpp"
 #include "reading.hpp"
+#include "../top/top.hpp"
 
 using namespace std;
 
-string traceDirPath = "/home/pablo/Escritorio/PredicMem25/traces/";
-string inputBufferTracesDirName = "inputBufferTraces/";
-string dictionaryTracesDirName = "dictionaryTraces/";
-string svmTracesDirName = "svmTraces/";
+string traceDirPath = "//home//pablo//Escritorio//PredicMem25//traces//";
+string inputBufferTracesDirName = "inputBufferTraces//";
+string dictionaryTracesDirName = "dictionaryTraces//";
+string svmTracesDirName = "svmTraces//";
 
+bool validateInputBuffer = false, validateDictionary = true, validateSVM = false;
 
 int main()
 {
-	auto inputBufferValidation = Experimentation<InputBufferSoftValidation>(traceDirPath + inputBufferTracesDirName);
-	auto dictionaryValidation = Experimentation<DictionarySoftValidation>(traceDirPath + dictionaryTracesDirName);
-	auto svmValidation = Experimentation<SVMSoftValidation>(traceDirPath + svmTracesDirName);
 
-	bool passed = // inputBufferValidation.perform();
-			dictionaryValidation.perform()
-			// && svmValidation.perform()
-			;
+	bool passed = true;
+
+	if(validateInputBuffer){
+		auto inputBufferValidation = Experimentation<InputBufferSoftValidation>(traceDirPath + string("inputBufferTrace.txt"));
+		auto experiments = inputBufferValidation.experiments;
+		for(auto& experiment : experiments){
+			for(int i = 0; i < experiment.getNumOperations(); i++){
+				auto input = experiment.getNextInput();
+				bool isHit = false;
+				InputBufferValidationOutput output;
+				auto entry = operateInputBuffer(input.inputBufferAddr, input.entry, input.performRead, isHit);
+				output.entry = entry;
+				output.isHit = isHit;
+				experiment.saveOutput(output);
+			}
+			passed = passed && experiment.hasPassed();
+		}
+	}
+
+
+	if(validateDictionary){
+		auto dictionaryValidation = Experimentation<DictionarySoftValidation>(traceDirPath + string("dictionaryTrace.txt"));
+		auto experiments = dictionaryValidation.experiments;
+		for(auto& experiment : experiments){
+			for(int i = 0; i < experiment.getNumOperations(); i++){
+				auto input = experiment.getNextInput();
+				bool isHit = false;
+				dic_index_t resultIndex = 0;
+				DictionaryValidationOutput output;
+				auto entry = operateDictionary(input.index, input.delta, input.performRead, resultIndex, isHit);
+				output.entry = entry;
+				output.isHit = isHit;
+				output.resultIndex = resultIndex;;
+				experiment.saveOutput(output);
+			}
+			passed = passed && experiment.hasPassed();
+		}
+	}
+
+
+	if(validateSVM){
+		auto svmValidation = Experimentation<SVMSoftValidation>(traceDirPath + string("svmTrace.txt"));
+		auto experiments = svmValidation.experiments;
+		for(auto& experiment : experiments){
+			for(int i = 0; i < experiment.getNumOperations(); i++){
+				auto input = experiment.getNextInput();
+				class_t outputClasses[MAX_PREFETCHING_DEGREE];
+				SVMValidationOutput output;
+				operateSVM(input.input, input.target, outputClasses);
+				for(int k = 0; k < MAX_PREFETCHING_DEGREE; k++){
+					output.output[k] = outputClasses[k];
+				}
+				experiment.saveOutput(output);
+			}
+			passed = passed && experiment.hasPassed();
+		}
+	}
 
 	cout << "Passed: " << to_string(passed) << "\n";
 	return !passed;
